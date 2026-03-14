3bacf982a3f87969cef2a212ae2772b558e21a64
Aaron Wu <faw21@pitt.edu>
2026-03-13 17:57:37 -0700

    fix: load_dotenv override=True so ANTHROPIC_API_KEY isn't masked by empty env var

--- tests/test_integration_llm.py
+++ tests/test_integration_llm.py
@@ -10,7 +10,7 @@ import os
 import pytest
 from dotenv import load_dotenv
 
-load_dotenv("/Users/aaronwu/Local/my-projects/give-it-all/.env")
+load_dotenv("/Users/aaronwu/Local/my-projects/give-it-all/.env", override=True)
 
 from standup_ai.providers import create_provider, ClaudeProvider, OpenAIProvider, OllamaProvider
 
